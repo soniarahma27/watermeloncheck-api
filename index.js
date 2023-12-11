a9dc7caf6451d4586e51848e0b43d85a883a5d38
Sonia Rahmawati <ven.sonia@bankbsi.co.id>
2023-12-11 11:15:48 +0700

    edit some file for deploy vercel

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
 
-app.get('/helloworld', (req, res) => {
-  res.send('Hello World!')
-});
+const app = express()
+const PORT = 4000
 
-app.listen(port, () => {
-  console.log(`watermeloncheck-api listening at http://localhost:${port}`)
-});
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
